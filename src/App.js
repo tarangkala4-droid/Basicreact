86eb500cd67555c0bd23e803150b74ac754d059c
tarangkala4-droid <tarangkala4@gmail.com>
2025-11-19 08:25:15 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") return;
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className="text-4xl text-blue-600 font-bold text-center mt-10">
+  Tailwind is working! 🎉
+</div>
+
+  );
+  
+
+}
+
+export default App;
+
